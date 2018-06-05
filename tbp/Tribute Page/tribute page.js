d836e9ee22f7715cfe80eb9c76484eed76832289
TheBestPessimist <cristian.nahsuc@gmail.com>
2018-06-05 22:11:22 +0300

    Tribute page, v0.2

---
+++ tbp/Tribute Page/tribute page.js
@@ -0,0 +1,19 @@
+// /*
+// how difficult can it be to scroll down in an animated way
+// well it appears it it QUITE FUCKING DIFFICULT mate!
+//  */
+//
+// //.. eh, at least this seems to work in chrome screw other browsers :^)
+// function toBottom()
+// {
+//     // alert("Scrolling to bottom ...");
+//     window.scrollTo(0, document.body.scrollHeight);
+// }
+//
+// /*
+// this one calls the javascript code when the page load.
+//   what does that mean?
+//     - is the bloody page fully loaded, with all the things visible
+//     - is the said bloody page almost loaded, only some things are left (which???)
+//
+// window.onload=toBottom;
